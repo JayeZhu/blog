33adf667a34e0cba51fccb9d6b83d61b7ff7463a
JayeZhu <1032657951@qq.com>
2021-03-17 01:15:34 +0800

    text

---
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -0,0 +1,59 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '前端',
+    link: '/fe/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+      { text: 'HTML', link: '/fe/01d45e/'},
+      { text: 'CSS', link: '/fe/01d45e/'},
+      { text: 'JavaScript', link: '/fe/01d45e/'},
+    ],
+  },
+  {
+    text: '后端',
+    link: '/be/',
+    items: [
+      { text: 'Node', link: '/pages/8309a5b876fc95e3/' },
+    ],
+  },
+  {
+    text: '技术',
+    link: '/technology/',
+    items: [
+      { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+    ],
+  },
+  {
+    text: '更多',
+    link: '/more/',
+    items: [
+      { text: '算法', link: '/algorithm/e78365/' },
+      { text: '学习', link: '/pages/f2a556/' },
+      { text: '面试', link: '/pages/aea6571b7a8bae86/' },
+      { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
+      { text: '实用技巧', link: '/pages/baaa02/' },
+      { text: '友情链接', link: '/friends/' },
+    ],
+  },
+  { text: '关于', link: '/about/' },
+  {
+    text: '收藏',
+    link: '/pages/beb6c0bd8a66cea6/',
+    items: [
+      { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+      { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+      { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+    ],
+  },
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
+]
