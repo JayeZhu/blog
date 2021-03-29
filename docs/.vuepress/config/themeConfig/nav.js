4b55dae533f45a2a467a7c38aa84b331d0b9f14e
unknown <F1334606@systemplus.foxconn.com>
2021-03-29 11:47:23 +0800

    添加收藏

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -40,11 +40,11 @@ module.exports = [
   { text: '关于', link: '/about/' },
   {
     text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
+    link: '/collections/beb6c0bd8a66cea6/',
     items: [
-      { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-      { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-      { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+      { text: '网站', link: '/collections/4f748c/' },
+      { text: '文章', link: '/collections/c1978c/' },
+      { text: '应用', link: '/collections/0944a9/' },
     ],
   },
   {
